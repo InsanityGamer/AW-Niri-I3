1d5c059bb5b42624ea5040c9a289c0062c064a1e
AvalonWaveSoftware <Seanmm1997@gmail.com>
2025-10-18 09:37:16 -0500

    Add files via upload

---
+++ install-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Author: Sean M
+#Purpose: Installs config files and packages automatically from tty1 (start from most recent Fedora Server OS)
+#Usage: ./install-config.sh
+#Instruction: Ensure the script is placed in user home directoy.
+#Pass a list into dnf package manager using Xargs as a handler
+cat ./dnflist3 | xargs sudo dnf install ;
+#flathub repo added
+flatpak remote-add --if-not-exists flathub	https://dl.flathub.org/repo/flathub.flatpakrepo ;
+#install obsidian
+flatpak install app/md.obsidian.Obsidian/x86_64/stable -y ;
+#Setting user to gui target
+sudo systemctl set-default graphical.target ;
+#Copying configs into USER home folder
+cp -R ./.config $HOME/ ;
+cp ./.bashrc $HOME/ ;
+sudo cp ./config.toml /etc/greetd/config.toml
+sudo cp ./xinitrc /etc/X11/xinit/xinitrc 
+sudo systemctl enable greetd --now;
